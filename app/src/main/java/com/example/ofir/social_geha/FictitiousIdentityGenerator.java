d2cb374c23111901d95eadda9a2a2191f8b36f1f
Itamaror <itamar.ord@campus.technion.ac.il>
2019-04-27 23:04:21 +0300

    Another small fix

--- app/src/main/java/com/example/ofir/social_geha/FictitiousIdentityGenerator.java
+++ app/src/main/java/com/example/ofir/social_geha/FictitiousIdentityGenerator.java
@@ -21,13 +21,13 @@ public class FictitiousIdentityGenerator {
             int randomAdjective = (int)(Math.random()*male_adjectives.length);
             int randomAnimal = (int)(Math.random()*male_animals.length);
             int randomColor = 1 + (int)(Math.random()*numberColor);
-            name = male_adjectives[randomAdjective] + male_animals[randomAnimal];
+            name = male_adjectives[randomAdjective] + " " + male_animals[randomAnimal];
             randomPicture = male_animals_eng[randomAnimal] + "_" + randomColor + ".jpg";
         } else {
             int randomAdjective = (int)(Math.random()*female_adjectives.length);
             int randomAnimal = (int)(Math.random()*male_animals.length);
             int randomColor = 1 + (int)(Math.random()*numberColor);
-            name = female_adjectives[randomAdjective] + female_animals[randomAnimal];
+            name = female_adjectives[randomAdjective]+ " " + female_animals[randomAnimal];
             randomPicture = female_animals_eng[randomAnimal] + "_" + randomColor + ".jpg";
         }
         return new AnonymousIdentity(name, randomPicture);
