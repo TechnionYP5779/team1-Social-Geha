dd83c9336c9af2caa8cd5849647ab45315ef9aab
Shai Zeevi <36810747+ShaiZeevi@users.noreply.github.com>
2019-05-06 13:26:25 +0300

    added back button and margins to zoom #45

--- app/src/main/java/com/example/ofir/social_geha/Activities_and_Fragments/ZoomedPictureActivity.java
+++ app/src/main/java/com/example/ofir/social_geha/Activities_and_Fragments/ZoomedPictureActivity.java
@@ -1,8 +1,11 @@
 package com.example.ofir.social_geha.Activities_and_Fragments;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.example.ofir.social_geha.R;
@@ -10,13 +13,24 @@ import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class ZoomedPictureActivity extends AppCompatActivity {
-
+    Toolbar mToolbar;
     String imageURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zoomed_picture);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar2);
+
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_left_white_24dp);
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), AllChatsActivity.class));
+            }
+        });
 
         //display the image from URL
         ImageView imgView = findViewById(R.id.zoomed_profile_picture);
