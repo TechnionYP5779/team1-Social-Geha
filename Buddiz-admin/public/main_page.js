bc9b7f64ccf6469aa3622a656e4c4f6047055b5c
unknown <matan.pugach@campus.technion.ac.il>
2019-06-02 14:57:07 +0300

    unnecessary coe elimination #64

--- Buddiz-admin/public/main_page.js
+++ Buddiz-admin/public/main_page.js
@@ -3,8 +3,13 @@ $( function() {
       emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
       name = $( "#name" ),
       allFields = $( [] ).add( name ),
-	  userCode = "";
- 
+	  phoneNumber = "",
+        kind = "",
+        department = "",
+	  userCode = "",
+     database = firebase.database();
+    ;
+
     function updateTips( t ) {
       tips
         .text( t )
@@ -36,26 +41,30 @@ $( function() {
     }
  
     function addUser() {
-      var valid = true;
-      allFields.removeClass( "ui-state-error" );
- 
-      valid = valid && checkLength( name, "username", 3, 16 );
-      valid = valid && checkLength( email, "email", 6, 80 );
-      valid = valid && checkLength( password, "password", 5, 16 );
- 
-      valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
-      valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
-      valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
- 
-      if ( valid ) {
-        $( "#users tbody" ).append( "<tr>" +
-          "<td>" + name.val() + "</td>" +
-          "<td>" + email.val() + "</td>" +
-          "<td>" + password.val() + "</td>" +
-        "</tr>" );
-        dialog_add.dialog( "close" );
-      }
-      return valid;
+        firebase.database().ref('adminAddedUsers/' + 55555).set({
+            companyName: "hello",
+            contact: "hello",
+        })
+      // var valid = true;
+      // allFields.removeClass( "ui-state-error" );
+      //
+      // valid = valid && checkLength( name, "username", 3, 16 );
+      // valid = valid && checkLength( email, "email", 6, 80 );
+      // valid = valid && checkLength( password, "password", 5, 16 );
+      //
+      // valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
+      // valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
+      // valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+      //
+      // if ( valid ) {
+      //   $( "#users tbody" ).append( "<tr>" +
+      //     "<td>" + name.val() + "</td>" +
+      //     "<td>" + email.val() + "</td>" +
+      //     "<td>" + password.val() + "</td>" +
+      //   "</tr>" );
+      //   dialog_add.dialog( "close" );
+      // }
+      // return valid;
     }
 	
 	function findUser() {
