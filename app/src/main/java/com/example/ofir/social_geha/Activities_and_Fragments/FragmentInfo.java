cfcc14c7fa2b597a51b111814fda41f6681e42b9
Ofir <ofiralexi@gmail.com>
2019-06-03 20:20:30 +0300

    fixed UI

---
+++ app/src/main/java/com/example/ofir/social_geha/Activities_and_Fragments/FragmentInfo.java
@@ -0,0 +1,29 @@
+package com.example.ofir.social_geha.Activities_and_Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ofir.social_geha.Activities_and_Fragments.activity_main_drawer;
+import com.example.ofir.social_geha.R;
+
+
+public class FragmentInfo extends Fragment {
+    public FragmentInfo() { }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ((activity_main_drawer)getActivity()).setActionBarTitle("אודות");
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+
+}
