f7025c8162a754f8cabaf23ca7608b318a2cd4c2
Shai Zeevi <36810747+ShaiZeevi@users.noreply.github.com>
2019-06-13 22:37:16 +0300

    documented allChatsFragment #112

--- app/src/main/java/com/example/ofir/social_geha/Activities_and_Fragments/AllChatsFragment.java
+++ app/src/main/java/com/example/ofir/social_geha/Activities_and_Fragments/AllChatsFragment.java
@@ -3,16 +3,13 @@ package com.example.ofir.social_geha.Activities_and_Fragments;
 import android.content.Intent;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -27,15 +24,10 @@ import com.example.ofir.social_geha.Firebase.Database;
 import com.example.ofir.social_geha.Firebase.Message;
 import com.example.ofir.social_geha.Person;
 import com.example.ofir.social_geha.R;
-import com.github.angads25.toggle.interfaces.OnToggledListener;
-import com.github.angads25.toggle.model.ToggleableView;
 import com.github.angads25.toggle.widget.DayNightSwitch;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -77,6 +69,7 @@ public class AllChatsFragment extends Fragment {
         FloatingActionButton fab = v.findViewById(R.id.new_conversation_fab);
         fab.setOnClickListener(v1 -> startActivity(new Intent(getActivity(), FilterMatchesActivity.class)));
 
+        //find the current person in the DB
         Database.getInstance().getdb().collection("users").whereEqualTo("userID", Database.getInstance().getLoggedInUserID()).get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
@@ -87,10 +80,10 @@ public class AllChatsFragment extends Fragment {
 
                 });
 
-
         setupToggle(v);
         loadList();
 
+        //makes sure that when a chat item is pressed, we're transferred to the appropriate chat with this person
         mListView.setOnItemClickListener((parent, view, position, id) -> {
             Object o = mListView.getItemAtPosition(position);
             ChatEntry chatEntry = (ChatEntry) o; //As you are using Default String Adapter
@@ -105,14 +98,15 @@ public class AllChatsFragment extends Fragment {
         });
 
 
-        //Set long click listener
+        //Set long click listener on each chat item
+        //a long click pops up the sharing menu
         mListView.setLongClickable(true);
         mListView.setOnItemLongClickListener((arg0, arg1, pos, id) -> {
 
+            //the id of the person we've clicked
             String uid = ((activity_main_drawer) getActivity()).conversationList.get(pos).getUserID();
-            //cannot click once it's been shared
-            //cannot click once it's been shared
-            //Toast.makeText(getActivity(), sharingsFileHandler.getUIDs().toString(), Toast.LENGTH_LONG).show();
+            //this loop makes sure that we cannot long-click a chat the information has been shared with that person
+            //that's because we've decided that once information has been shared, there is no way to take it back
             for (String ID : sharingsFileHandler.getUIDs()) {
                 if (uid.equals(ID)) {
                     String toastText = "לא ניתן לבטל את שיתוף המידע עם " + ((activity_main_drawer) getActivity()).conversationList.get(pos).getName();
@@ -121,15 +115,7 @@ public class AllChatsFragment extends Fragment {
                 }
             }
 
-//            Map<String, ContactListFileHandler.Contact> contactMap = new ContactListFileHandler(getActivity()).getContacts();
-//            for (Map.Entry<String, ContactListFileHandler.Contact> c : contactMap.entrySet()) {
-//                if (c.getValue().getUid().equals(uid) && !c.getValue().getRealName().equals(ContactListFileHandler.Contact.UNKNOWN_NAME)) {
-//                    String toastText = "לא ניתן לבטל את שיתוף המידע עם " + c.getValue().getRealName();
-//                    Toast.makeText(getActivity(), toastText, Toast.LENGTH_SHORT).show();
-//                    return true;
-//                }
-//            }
-
+            //sets up the dialog
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             LayoutInflater inflater1 = getLayoutInflater();
             View dialogView = inflater1.inflate(R.layout.alert_dialog_information_exposre, null);
@@ -137,21 +123,23 @@ public class AllChatsFragment extends Fragment {
             Button one = dialogView.findViewById(R.id.button1);
             Button three = dialogView.findViewById(R.id.button3);
             final AlertDialog dialog = builder.create();
+
+            //the red button merely dismissed the dialog without doing anything
             one.setOnClickListener(v12 -> {
                 Toast.makeText(getActivity(), "בוטל", Toast.LENGTH_SHORT).show();
                 dialog.dismiss();
             });
 
+            //the "ok" button, shares the information by sending a control message to that person with our real name (which his listener will then interpret & record)
             three.setOnClickListener(v12 -> {
                 String content = "IDENTITY$" + Database.getInstance().getLoggedInUserID() + "#" + p.getRealName();
-                //SharingsFileHandler sharingsFileHandler = new SharingsFileHandler(getActivity());
                 sharingsFileHandler.addUID(((activity_main_drawer) getActivity()).conversationList.get(pos).getUserID());
                 Database.getInstance().sendControlMessage(content, Database.getInstance().getLoggedInUserID(), ((activity_main_drawer) getActivity()).conversationList.get(pos).getUserID());
                 Toast.makeText(getActivity(), "שותף בהצלחה", Toast.LENGTH_SHORT).show();
                 dialog.dismiss();
-                //Toast.makeText(getActivity(), "sharings = " + sharingsFileHandler.getUIDs().toString(), Toast.LENGTH_LONG).show();
             });
 
+            //for an added layer of protection against accidental clicks, we make sure the user knows the implications of pressing "ok"
             String are_you_sure_msg = "האם אתה בטוח שברצונך לשתף את זהותך עם " + ((activity_main_drawer) getActivity()).conversationList.get(pos).getName() + "?";
             TextView message = dialogView.findViewById(R.id.textView2);
             message.setText(are_you_sure_msg);
@@ -166,6 +154,9 @@ public class AllChatsFragment extends Fragment {
     }
 
 
+    /**
+     * Provides the necessary initialization for the two conversation lists
+     */
     private void loadList() {
         //Create the list of objects
         ((activity_main_drawer) getActivity()).conversationList = new ArrayList<>();
@@ -177,9 +168,10 @@ public class AllChatsFragment extends Fragment {
     }
 
 
+    /**
+     * Adds a listener which deals with new incoming messages. Then, populates the list to display to the user
+     */
     private void setMessageListeners() {
-        Log.d("SHAI", "IN POPULATE CONV LIST");
-
         mListener = mFirestore.collection(MESSAGES).whereEqualTo("toPersonID", Database.getInstance().getLoggedInUserID())
                 .addSnapshotListener((queryDocumentSnapshots, e) -> {
                     if (e != null) {
@@ -188,39 +180,38 @@ public class AllChatsFragment extends Fragment {
 
                     for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
                         if (doc.getType() == DocumentChange.Type.ADDED) {
-                            Message message = doc.getDocument().toObject(Message.class); //added message
+                            Message message = doc.getDocument().toObject(Message.class); //this is the new message that was added
                             mFirestore.collection(MESSAGES).document(doc.getDocument().getId()).delete();
                             // decrypt regular messages
                             String contactUID = message.getFromPersonID();
                             if (message.getShown()) {
+                                //A shown message is decrypted
                                 KeyFileHandler keyFileHandler = new KeyFileHandler(getActivity(), contactUID);
                                 AES aes = new AES(keyFileHandler.getKey());
                                 message.setMessage(aes.decrypt(message.getMessage()));
                             }
 
-//                                if(message.getMessage() == null || message.getMessage().equals("CHAT REQUEST$")
-//                                        || message.getMessage().equals("CHAT ACCEPT$") || message.getMessage().equals("CHAT REJECT$")) continue;
+                            //every message is written to the file
                             mFileHandler.writeMessage(message); //non shown messages will be ignored anyway but may as well write them
 
                             if (message.getShown()) {
                                 continue;
                             }
 
-                            //it's a control message - our business
+                            //it's a control message
                             String text = message.getMessage();
 
-                            /* self message*/
+                            /* self message - we skip*/
                             if (contactUID.equals(Database.getInstance().getLoggedInUserID()))
                                 continue;
 
-                            /*a control message but not an identity reveal*/
+                            /*a control message which indicates someone has revealed their information to us*/
                             if (text.substring(0, "IDENTITY".length()).equals("IDENTITY")) {
+                                //identify the sensitive info & write it in the Contact
                                 String realName = text.substring(text.indexOf('#') + 1);
-                                Log.d("SHAI", "got control message from " + message.getFromPersonID());
-                                Log.d("SHAI", "his/her realname is " + realName);
                                 mContactsHandler.changeName(contactUID, realName);
 
-                                //for the full effect - swap the names in the lists
+                                //for the full effect - swap the names in the lists to display the correct name
                                 //The contact must be in the list since a first message can not be an identity reveal one
                                 for (int i = 0; i < ((activity_main_drawer) getActivity()).conversationList.size(); i++) {
                                     if (((activity_main_drawer) getActivity()).conversationList.get(i).getUserID().equals(contactUID)) {
@@ -228,21 +219,23 @@ public class AllChatsFragment extends Fragment {
                                     }
                                 }
 
+                                //switch in both lists, the full one & the displayed one (for searching)
                                 for (int i = 0; i < ((activity_main_drawer) getActivity()).allList.size(); i++) {
                                     if (((activity_main_drawer) getActivity()).allList.get(i).getUserID().equals(contactUID)) {
                                         ((activity_main_drawer) getActivity()).allList.get(i).setRealName(realName);
                                     }
                                 }
-                                // a message we should ignore because it's not an identity reveal - to the next change
+
+                                //a key exchange message, we record the AES key in the appropriate file
                             } else if (text.substring(0, "AES".length()).equals("AES")) {
                                 KeyFileHandler keyFileHandler = new KeyFileHandler(getActivity(), contactUID);
                                 keyFileHandler.writeKey(AES.stringToKey(text.substring("AES".length())));
-                                Log.d("AES_READ", "received key from " + contactUID + " and the key is " + text.substring("AES".length()));
+                                // a name change message, indicating someone who has revealed their name to us has now changed it - we record the new name in the contact
                             } else if (text.substring(0, "NAME_CHANGE".length()).equals("NAME_CHANGE")) {
                                 String realName = text.substring(text.indexOf('#') + 1);
-                                Log.d("NAME_CHANGE", "received a name change from " + contactUID + " to change name to " + realName);
                                 mContactsHandler.changeName(contactUID, realName);
 
+                                //update the new name in the two lists for an immediate effect
                                 for (int i = 0; i < ((activity_main_drawer) getActivity()).conversationList.size(); i++) {
                                     if (((activity_main_drawer) getActivity()).conversationList.get(i).getUserID().equals(contactUID)) {
                                         ((activity_main_drawer) getActivity()).conversationList.get(i).setRealName(realName);
@@ -259,20 +252,28 @@ public class AllChatsFragment extends Fragment {
                         }
 
                     }
+
+                    //populates the list on a first call
                     updateList();
                 });
     }
 
+    /**
+     * This function makes sure the list is updated with new conversation that might have arrived
+     */
     public void updateList() {
         String loggedInUserID = Database.getInstance().getLoggedInUserID();
         Map<String, ChatEntry> chatEntryMap = new HashMap<>();
         Map<String, ContactListFileHandler.Contact> contactMap = new ContactListFileHandler(getActivity()).getContacts();
         final String dummyName = "CONTACT DUMMY";
 
+        //initializes an empty mapping between id's and contacts in the contact-list (in order to save get each contact's last message to display)
         for (Map.Entry<String, ContactListFileHandler.Contact> contact : contactMap.entrySet()) {
             chatEntryMap.put(contact.getKey(), new ChatEntry(contact.getValue(), null, 0));
         }
 
+        //fills in the mapping from an id to the last shown message sent by that person
+        //any non-contact encountered is added to the contact-list file & the mapping
         for (Message msg : mFileHandler.getMessages()) {
             if (!msg.getShown()) continue; //ensures that the last SHOWN message is in the map
             ChatEntry chatEntry;
@@ -297,8 +298,11 @@ public class AllChatsFragment extends Fragment {
             }
         }
 
+        //remove the logged in user from the list
         chatEntryMap.remove(loggedInUserID);
 
+        //traverses the mapping we've created, if the screen already displays a chat from that contact it replaces the message to the new one
+        //if it's a new person, it adds a box with this new chat to the list on the screen - hence, updating it.
         for (final Map.Entry<String, ChatEntry> entry : chatEntryMap.entrySet()) {
 
             if (!entry.getValue().getName().equals(dummyName)) {
@@ -321,9 +325,8 @@ public class AllChatsFragment extends Fragment {
                 Task<QuerySnapshot> personQueryTask = mFirestore.collection(USERS).whereEqualTo("userID", entry.getKey()).get();
                 personQueryTask.addOnCompleteListener(task -> {
                     QuerySnapshot person = task.getResult();
-                    if (!person.isEmpty()) {
+                    if (person != null && !person.isEmpty()) {
                         Person myPerson = person.toObjects(Person.class).get(0);
-                        Log.d("COOLTEST", "got user: " + myPerson.getDescription());
                         ContactListFileHandler.Contact myContact = new ContactListFileHandler.Contact(myPerson.getUserID(), ContactListFileHandler.Contact.UNKNOWN_NAME,
                                 myPerson.getDescription(), myPerson.getAnonymousIdentity(), new Date(Long.MIN_VALUE));
                         ChatEntry chatEntry = new ChatEntry(myContact, entry.getValue().getMessage(), entry.getValue().getUnreadCount()); //contact & message might have been updated
@@ -342,7 +345,6 @@ public class AllChatsFragment extends Fragment {
                         ((activity_main_drawer) getActivity()).mAdapter.notifyDataSetChanged();
 
                         //since this person is not in the contactList we to update the file
-                        Log.d("SHAI", "NEW CONTACT");
                         mContactsHandler.addContact(myContact);
                     }
                 });
@@ -365,6 +367,11 @@ public class AllChatsFragment extends Fragment {
         updateList();
     }
 
+    /**
+     * @param list - a list of chat entries
+     * @param uid - an id of a user
+     * @return - the index of a an entry whose id matches @param uid if one exists (or -1 otherwise)
+     */
     int listContains(List<ChatEntry> list, String uid) {
         for (int i = 0; i < list.size(); i++) {
             ChatEntry ce = list.get(i);
@@ -373,6 +380,9 @@ public class AllChatsFragment extends Fragment {
         return -1;
     }
 
+    /**
+     * Setup the availability toggle
+     */
     private void setupToggle(View v) {
         DayNightSwitch dayNightSwitch = v.findViewById(R.id.toggle_availability);
         TextView textView = v.findViewById(R.id.description_availability);
