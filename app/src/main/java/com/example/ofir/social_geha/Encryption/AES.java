5d2a6eddfb2b92699ae635f167b9fd19208b81f0
yoavz <yoavzuriel@cs.technion.ac.il>
2019-06-16 10:56:23 +0300

    AES Documentation #115

--- app/src/main/java/com/example/ofir/social_geha/Encryption/AES.java
+++ app/src/main/java/com/example/ofir/social_geha/Encryption/AES.java
@@ -8,17 +8,29 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
+/***
+ * Class used to encrypt messages using a symmetric encryption method
+ */
 public class AES {
     public SecretKey key;
 
+    /***
+     * Creates a new instance of AES encrypt/decrypt
+     * @param keySize can be 128, 192 or 256 bits
+     */
     public AES(int keySize) {
         key = genKeys(keySize);
     }
 
+    /***
+     * Use this when reading a key from a file to create a new instance of AES
+     * @param key the key read from the file
+     */
     public AES(SecretKey key) {
         this.key = key;
     }
 
+    /// Service methods to transform byte to String
     private static byte[] toByte(String hexString) {
         int len = hexString.length() / 2;
         byte[] result = new byte[len];
@@ -44,6 +56,12 @@ public class AES {
         sb.append(HEX.charAt((b >> 4) & 0x0f)).append(HEX.charAt(b & 0x0f));
     }
 
+    /***
+     * Encrypts the message using the instance's key
+     * @param message the message to encrypt
+     * @return the encrypted message in hex encoding
+     *          This is a String so it can be sent and read without problem
+     */
     public String encrypt(String message) {
         try {
             Cipher cipher = Cipher.getInstance("AES");
@@ -54,6 +72,11 @@ public class AES {
         }
     }
 
+    /***
+     * Decrypts the cipher text using the instance's key
+     * @param cipherText an encrypted message in the hex format used above
+     * @return the decrypted message
+     */
     public String decrypt(String cipherText) {
         try {
             Cipher cipher = Cipher.getInstance("AES");
@@ -64,6 +87,7 @@ public class AES {
         }
     }
 
+    /// Service method to create new key
     private static SecretKey genKeys(int keySize) {
         try {
             KeyGenerator aesGen = KeyGenerator.getInstance("AES");
@@ -74,10 +98,12 @@ public class AES {
         }
     }
 
+    /// translate SecretKey to String
     public static String keyToString(SecretKey key) {
         return Base64.encodeToString(key.getEncoded(), Base64.DEFAULT);
     }
 
+    /// translate String (read from a file) to SecretKey
     public static SecretKey stringToKey(String key) {
         byte[] encodedKey = Base64.decode(key, Base64.DEFAULT);
         return new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
