ae9de55554411fffba71e9b2eda8110c5b97f7d8
Yarin Shlafir <shlafiry@gmail.com>
2019-06-19 00:40:17 +0300

    AdminGivenData Documentation #114

--- app/src/main/java/com/example/ofir/social_geha/AdminGivenData.java
+++ app/src/main/java/com/example/ofir/social_geha/AdminGivenData.java
@@ -3,6 +3,10 @@ package com.example.ofir.social_geha;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * Encapsulates the data the admin assigned to a user on the admin side website
+ * This class is a fancy POJO (because it can be reconstructed by Firebase)
+ */
 public class AdminGivenData implements Serializable {
     private String id;
     private String name;
